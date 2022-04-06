2c228eb23b7e0c80bce4d94d798c3ba76c2fe6e1
gauri <gauriajay18@gmail.com>
2022-04-06 15:22:09 +0530

    db relationships

--- accounts/models.py
+++ accounts/models.py
@@ -9,18 +9,30 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+class Tag(models.Model):
+    name= models.CharField(max_length=100, null=True)
+    def __str__(self):
+        return self.name
 class Product(models.Model):
     CATEGORY=(('Indoor','Indoor'),
     ('Out door','Out door'))
     name= models.CharField(max_length=100, null=True)
     price= models.FloatField(null=True)
-    description= models.CharField(max_length=100, null=True)
+    description= models.CharField(max_length=100, null=True,blank=True)
     category= models.CharField(max_length=100, null=True,choices=CATEGORY)
     date= models.DateTimeField(auto_now_add=True, null=True)
+    tags=models.ManyToManyField(Tag)
+    def __str__(self):
+        return self.name
+    
 class Order(models.Model):
     STATUS=(('not yet delivered','not yet deliverd'),
     ('out for delivery','out for delivery'),
     ('delivered','delivered'),
     )
+    customer=models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
     date= models.DateTimeField(auto_now_add=True, null=True)
-    status= models.CharField(max_length=100, null=True)
+    status= models.CharField(max_length=100, null=True,choices=STATUS)
+  
