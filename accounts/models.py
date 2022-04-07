511a029ba30c81f1067e2993a8f6eeffbca56065
gauri <gauriajay18@gmail.com>
2022-04-07 15:33:58 +0530

    update and delete

--- accounts/models.py
+++ accounts/models.py
@@ -36,3 +36,5 @@ class Order(models.Model):
     date= models.DateTimeField(auto_now_add=True, null=True)
     status= models.CharField(max_length=100, null=True,choices=STATUS)
   
+    def __str__(self):
+        return self.product.name
