b03df4e4941aafa7ae2c2042767c8d634f8fde21
gauri <gauriajay18@gmail.com>
2022-04-09 10:11:06 +0530

    filter

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+
 from .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
 
 # Create your views here.
+def registerPage(request):
+    context={}
+    return render(request,'accounts/register.html',context)
+def loginPage(request):
+    context={}
+    return render(request,'accounts/login.html',context)
 def home(request):
     orders=Order.objects.all()
     customers=Customer.objects.all()
@@ -21,40 +29,43 @@ def customers(request,pk_test):
     customer=Customer.objects.get(id=pk_test)
     orders=customer.order_set.all()
     order_count=orders.count()
-    context={'customer':customer,'orders':orders,'order_count':order_count}
+    myFilter=OrderFilter(request.GET, queryset=orders)
+    orders=myFilter.qs
+    context={'customer':customer,'orders':orders,'order_count':order_count,'myFilter':myFilter}
     return render(request,'accounts/customers.html',context)
 
 def createOrder(request):
-	form = OrderForm()
-	if request.method == 'POST':
-		form = OrderForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('/')
+    form = OrderForm()
+    if request.method == 'POST':
+        
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
 
-	context = {'form':form}
-	return render(request, 'accounts/order_form.html', context)
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
 
-	order = Order.objects.get(id=pk)
-	form = OrderForm(instance=order)
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
 
-	if request.method == 'POST':
-		form = OrderForm(request.POST, instance=order)
-		if form.is_valid():
-			form.save()
-			return redirect('/')
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
 
-	context = {'form':form}
-	return render(request, 'accounts/order_form.html', context)
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
 
 
 def deleteOrder(request, pk):
-	order = Order.objects.get(id=pk)
-	if request.method == "POST":
-		order.delete()
-		return redirect('/')
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
 
-	context = {'item':order}
-	return render(request, 'accounts/delete.html', context)
+    context = {'item':order}
+    return render(request, 'accounts/delete.html', context)
